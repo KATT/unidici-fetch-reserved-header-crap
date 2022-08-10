cdfff6450f7d9965e94aef7a06f4d5490586d4e0
KATT <alexander@n1s.se>
2022-08-11 00:27:19 +0200

    here’s the actual fix

---
+++ utils/omit.ts
@@ -0,0 +1,17 @@
+export function omit<
+  TObj extends Record<string, unknown>,
+  TKey extends keyof TObj,
+>(obj: TObj, ...keys: TKey[] | [TKey[]]): Omit<TObj, TKey> {
+  const actualKeys: string[] = Array.isArray(keys[0])
+    ? (keys[0] as string[])
+    : (keys as string[]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const newObj: any = Object.create(null);
+  for (const key in obj) {
+    if (!actualKeys.includes(key)) {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
